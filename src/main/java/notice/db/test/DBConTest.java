79b50074bb063657a02d6090b8973a3b6f3deb39
pparkju <pparkjh90@gmail.com>
2022-06-22 11:02:20 +0900

    DBConTestStep2

--- src/main/java/notice/db/test/DBConTest.java
+++ src/main/java/notice/db/test/DBConTest.java
@@ -1,5 +1,24 @@
 package notice.db.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
 public class DBConTest {
 
+	//db접속 단위테스트
+		@Test
+		public void testDbcon() throws Exception {
+				
+			String driver = "oracle.jdbc.driver.OracleDriver";
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "hr";
+			String pw = "123456";
+
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url, user, pw);
+			
+			
+		}
 }
